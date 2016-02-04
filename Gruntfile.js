dd9ba3a5edffc03e0d3b3994e23a1bd8d1082610
HansHammel <oh.schitt@freenet.de>
2016-02-04 09:47:11 +0100

    added test, grunt tasks, etc.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,16 +6,23 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
+		copy: {
+			main: {
+				src: 'pjax-standalone.js',
+				dest: 'test/pjax-standalone.js'
+			}
+		},
 		jshint: {
     		ignore_warning: {
     		  src: ['pjax-standalone.js'],
 		      options: {
-		        '-W061': true, // eval is needed
-		      },
+		        '-W061': true // eval is needed
+		      }
 		    }
 	  	},
 	  	uglify: {
@@ -34,6 +41,6 @@ module.exports = function(grunt) {
 
 	});
 
-	grunt.registerTask('default', ['jshint','uglify']);
-}
+	grunt.registerTask('default', ['jshint','uglify','copy']);
+};
 
---
+++ test/pjax.spec.js
@@ -0,0 +1,75 @@
+var shelljs = require('shelljs');
+
+if (!shelljs.which('php')) {
+    shelljs.echo('php not found please install and put in your path');
+    process.exit(1);
+}
+
+
+var child = shelljs.exec('php -S 0.0.0.0:8000 -c test/php.ini -t test', {async:true});
+child.stdout.on('data', function(data) {
+    console.log(data);
+});
+child.stderr.on('data', function(data) {
+    console.error(data);
+});
+
+
+var Browser = require('zombie');
+var assert  = require('assert');
+
+// We're going to make requests to http://example.com/signup
+// Which will be routed to our test server localhost:3000
+Browser.localhost('localhost', 8000);
+
+
+describe('PJAX Page', function() {
+
+    var browser = new Browser();
+    browser.debug();
+    browser.runScripts = true;
+    browser.on('error',function (err){console.log(err)});
+
+    before(function(done) {
+        browser.visit('/', done);
+    });
+
+    describe('navigate', function() {
+/*
+        before(function(done) {
+            browser
+                .fill('email',    'zombie@underworld.dead')
+                .fill('password', 'eat-the-living')
+                .pressButton('Sign Me Up!', done);
+        });
+        */
+
+        it('should be successful', function(done) {
+            browser.assert.success();
+            done();
+        });
+
+        it('should see welcome page', function(done) {
+            browser.assert.text('title', 'PJAX-Standalone');
+            done();
+        });
+
+        /*
+        it('should click', function(done) {
+            browser.clickLink('body > div.navbar.navbar-default.navbar-static-top > ul > li:nth-child(2) > a', function(){
+                    console.log('link followed');
+                });
+            browser.wait({ duration: 100 });
+            assert.equal(browser.location.href, 'http://localhost/page1.php');
+            done();
+        });
+        */
+
+        it('should cause no errors', function(done) {
+            console.log(browser.errors);
+            assert.equal( browser.errors.length, 0);
+            done();
+        });
+
+    });
+});
